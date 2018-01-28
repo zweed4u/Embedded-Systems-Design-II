559d07f83b762f1b2019540b968c89839e3b7066
Zachary Weeden <zdw7287@rit.edu>
2018-01-28 14:44:51 -0500

    Fixing logic for opposite direction encoders, removed hardcoded encoder tuples for nonmatching encoder ticks where left forward, right backward, and added logic for opposite direction encoders when ticks match

--- week2/lab1_adv.py
+++ week2/lab1_adv.py
@@ -237,27 +237,29 @@ class Rover(QtGui.QGraphicsItem):
             elif left_dir == 1 and right_dir == 0:
                 if left_ticks != right_ticks:
                     print "Rotating"
-                    if (left_ticks, right_ticks) == (0, 1):
+                    if left_ticks + 1 == right_ticks:
                         self.angle += 45
                         self.rotate(45.0)
-                    elif (left_ticks, right_ticks) == (0, 2):
+                    elif left_ticks + 2 == right_ticks:
                         self.angle += 90
                         self.rotate(90.0)
-                    elif (left_ticks, right_ticks) == (1, 0):
+                    elif left_ticks == right_ticks + 1:
                         self.angle += 45
                         self.rotate(45.0)
-                    elif (left_ticks, right_ticks) == (1, 2):
-                        self.angle += 45
-                        self.rotate(45.0)
-                    elif (left_ticks, right_ticks) == (2, 0):
+                    elif left_ticks == right_ticks + 2:
                         self.angle += 90
                         self.rotate(90.0)
-                    elif (left_ticks, right_ticks) == (2, 1):
-                        self.angle += 45
-                        self.rotate(45.0)
                 else:
-                    # Left wheel forward and right wheel backward same encoder ticks - offset - stationary
-                    pass
+                    # Left wheel forward and right wheel backward same encoder ticks
+                    # if 0 and 0 - stationary
+                    # if 1 and 1 - +90 degrees right
+                    # if 2 and 2 - +180 degrees right
+                    if (left_ticks, right_ticks) == (1, 1):
+                        self.angle += 90
+                        self.rotate(90.0)
+                    elif (left_ticks, right_ticks) == (2, 2):
+                        self.angle += 180
+                        self.rotate(180.0)
             # Left wheels backward, right wheels forward
             elif left_dir == 0 and right_dir == 1:
                 if left_ticks != right_ticks:
@@ -275,8 +277,16 @@ class Rover(QtGui.QGraphicsItem):
                         self.angle -= 90
                         self.rotate(-90.0)
                 else:
-                    # Left wheel backward and right wheel forward same encoder ticks - offset - stationary
-                    pass
+                    # Left wheel backward and right wheel forward same encoder ticks
+                    # if 0 and 0 - stationary
+                    # if 1 and 1 - -90 degrees left
+                    # if 2 and 2 - -180 degrees left
+                    if (left_ticks, right_ticks) == (1, 1):
+                        self.angle -= 90
+                        self.rotate(-90.0)
+                    elif (left_ticks, right_ticks) == (2, 2):
+                        self.angle -= 180
+                        self.rotate(-180.0)
             self.instruction_step += 1
         else:
             print "Encoder text file fully traversed"
