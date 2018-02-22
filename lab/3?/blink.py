b0583674a29882560280a9862d97ae8eab283911
Zachary Weeden <zdw7287@rit.edu>
2018-02-22 17:36:07 -0500

    Added feb 22nd files and updated blink qtgui

--- lab/3?/blink.py
+++ lab/3?/blink.py
@@ -20,12 +20,14 @@ class MyWidget(QtGui.QWidget):
         # layout stuff
         button1 = QtGui.QPushButton("Toggle Me")
         button2 = QtGui.QPushButton("Blink Me")
-        button3 = QtGui.QPushButton("Stop")
+        button3 = QtGui.QPushButton("Clear")
         button4 = QtGui.QPushButton("Exit")
+        button5 = QtGui.QPushButton("Go High")
 
         vLayout = QtGui.QVBoxLayout()
         vLayout.addWidget(button1)
         vLayout.addWidget(button2)
+        vLayout.addWidget(button5)
         vLayout.addWidget(button3)
         vLayout.addWidget(button4)
         self.setLayout(vLayout)
@@ -46,22 +48,33 @@ class MyWidget(QtGui.QWidget):
         # slots
         button1.clicked.connect(lambda: self.toggle())
         button2.clicked.connect(lambda: self.blink_switch())
+        button5.clicked.connect(lambda: self.set_high())
         button3.clicked.connect(lambda: self.stop())
         button4.clicked.connect(lambda: self.exit())
         self.show()
 
     def exit(self):
+        print 'State: Exitting'
         self.stop()
         sys.exit()
 
     def blink_switch(self):
+        print 'State: Blink'
         self.blink = 1
 
     def stop(self):
+        print 'State: Low'
         self.blink = 0
         self.state = 0
         self.mem.seek(0)
         self.mem.write(struct.pack('l', 0))
+
+    def set_high(self):
+        print 'State: High'
+        self.blink = 0
+        self.mem.seek(0)
+        self.state = 1
+        self.mem.write(struct.pack('l', 1))
 
     def blink_me(self, sleep):
         poll_rate = .1
@@ -72,6 +85,7 @@ class MyWidget(QtGui.QWidget):
             time.sleep(poll_rate)
 
     def toggle(self):
+        # print 'State: Toggle' # causes to initial high - racecondition in thread start?
         self.mem.seek(0)
         if self.state == 0:
             self.state = 1
