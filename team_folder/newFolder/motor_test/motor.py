aab3ee193a83cbd7eb0b8262d5b5f3e1db5caec6
Zachary Weeden <zdw7287@rit.edu>
2018-02-28 20:09:33 -0500

    Reformatted code

--- team_folder/newFolder/motor_test/motor.py
+++ team_folder/newFolder/motor_test/motor.py
@@ -5,19 +5,19 @@ from mem_access import MemAccess
 
 
 class RoverMovement():
-	def __init__(self):
-		self.state = 0
-		self.direction = 0
-		self.front_left = 0
-		self.front_right = 0
-		self.rear_left = 0
-		self.rear_right = 0
+    def __init__(self):
+        self.state = 0
+        self.direction = 0
+        self.front_left = 0
+        self.front_right = 0
+        self.rear_left = 0
+        self.rear_right = 0
 
-	def move(self, reg, data, address):
-		MemAccess(reg=reg, data_to_mem=data, address_offset=address).write()
+    def move(self, reg, data, address):
+        MemAccess(reg=reg, data_to_mem=data, address_offset=address).write()
 
-	def rotate(self, reg, data, address):
-		MemAccess(reg=reg, data_to_mem=data, address_offset=address).write()
+    def rotate(self, reg, data, address):
+        MemAccess(reg=reg, data_to_mem=data, address_offset=address).write()
 
-	def fetch(self, reg, address):
-		MemAccess(reg=reg, address_offset=address).fetch()
+    def fetch(self, reg, address):
+        MemAccess(reg=reg, address_offset=address).fetch()
