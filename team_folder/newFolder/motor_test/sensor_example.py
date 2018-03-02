c151df6aaad05dfd32c4225f7feb0d2555a31e2e
Zachary Weeden <zdw7287@rit.edu>
2018-03-01 20:52:52 -0500

    Wrote up quick code implementation example for sensor

---
+++ team_folder/newFolder/motor_test/sensor_example.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# Zachary Weeden 2018
+
+import time
+import threading
+from mem_access import MemAccess
+
+# adjust accordingly
+sensor_trigger_pin = 0x41200010
+sensor_echo_pin = 0x41200014
+sensor_trigger_hold_microseconds = 10
+sensor_cycle_time_milliseconds = 60
+speed_of_sound_centimeters_per_second = 34000
+
+
+class Sensor:
+    def __init__(self):
+        self.time_delta = 0
+        self.sensor_thread = threading.Thread(target=self.trigger, args=(0,))
+        self.sensor_thread.daemon = True
+        self.sensor_thread.start()
+
+    def trigger(self, reg=0):
+        while 1:
+            MemAccess(reg=reg, data_to_mem=1,
+                      address_offset=sensor_trigger_pin).write()
+            time.sleep(sensor_trigger_hold_microseconds / 1000000)
+            MemAccess(reg=reg, data_to_mem=0,
+                      address_offset=sensor_trigger_pin).write()
+            while not self.fetch(sensor_echo_pin):
+                echo_pulse_tic = time.time()
+            while self.fetch(sensor_echo_pin):
+                echo_pulse_toc = time.time()
+            pulse_time = echo_pulse_toc - echo_pulse_tic
+            pulse_cm_distance = pulse_time * (
+                    speed_of_sound_centimeters_per_second / 2)
+            # logic on pulse_cm_distance here
+            time.sleep(sensor_cycle_time_milliseconds / 1000)
+
+    def fetch(self, address, reg=0):
+        return MemAccess(reg=reg, address_offset=address).fetch()
