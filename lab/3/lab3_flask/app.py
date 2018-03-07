c4712782b9e7f800bc21846b51c45301a7fbf1e6
Zachary Weeden <zdw7287@rit.edu>
2018-03-07 02:41:40 -0500

    Changed units to ms, fixed radio buttons and added condition to ensure pwm period input is greater than duty cycle input

--- lab/3/lab3_flask/app.py
+++ lab/3/lab3_flask/app.py
@@ -16,10 +16,12 @@ def submit():
     pwm_period = request.form['pwm_period']
     duty_cycle = request.form['duty_cycle']
     enable = request.form['enable']
-    print(f"PWM Period: {pwm_period}ns\nDuty Cycle: {duty_cycle}ns\nEnable: {enable}")
+    if int(duty_cycle) > int(pwm_period):
+        return json.dumps({'html':'<span>Duty Cycle must be less than PWM Period</span>'})
+    print(f"PWM Period: {pwm_period}ms\nDuty Cycle: {duty_cycle}ms\nEnable: {enable}")
     print(f"Duty Cycle %: {(float(duty_cycle)/float(pwm_period))*100.0}%")
-    duty_sleep = float(duty_cycle)/float(1000000000)  # seconds
-    period_sleep = float(pwm_period)/float(1000000000) - duty_sleep  # seconds
+    duty_sleep = float(duty_cycle)/float(1000)  # seconds
+    period_sleep = float(pwm_period)/float(1000) - duty_sleep  # seconds
     print(f"Write pin high and sleep for {duty_sleep} seconds, toggle the pin low as duty cycle has bee fufilled and now sleep remainder of period {period_sleep} seconds")
     if pwm_period and duty_cycle:
         return json.dumps({'html':'<span>PWM Period and Duty cycle fields good !!</span>'})
