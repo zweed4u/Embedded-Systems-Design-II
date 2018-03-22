6dbf5dc70b84242d142636a81b893414c60f73d5
Zachary Weeden <zdw7287@rit.edu>
2018-03-22 00:39:51 -0400

    Added graphics for initial python implementations, converted tabs to spaces and accounted for gearbox ratio in distance calculation method

--- team_folder/newFolder/Python/distance.py
+++ team_folder/newFolder/Python/distance.py
@@ -3,19 +3,19 @@ import math
 
 
 class TickConversion:
-	def __init__(self):
-		self.GEAR_BOX_RATIO = 87  # 86.8:1 (driven/driving) - # of teeth - for every 87 rotations of driving effort gear, driven load gear makes 1 rotation
-		self.ROVER_WHEEL_DIAMETER_MM = 60
-		self.ROVER_WHEEL_DIAMETER_CM = self.ROVER_WHEEL_DIAMETER_MM / 10.0
-		self.ROVER_WHEEL_DIAMETER_IN = self.ROVER_WHEEL_DIAMETER_CM / 2.54
-		self.SYS_CLK = 100000000
-		self.states_per_rotation = 1000 / 3.0  # From "Rover 5.pdf"
+    def __init__(self):
+        self.GEAR_BOX_RATIO = 87  # 86.8:1 (driven/driving) - # of teeth - for every 87 rotations of driving effort gear, driven load gear makes 1 rotation
+        self.ROVER_WHEEL_DIAMETER_MM = 60
+        self.ROVER_WHEEL_DIAMETER_CM = self.ROVER_WHEEL_DIAMETER_MM / 10.0
+        self.ROVER_WHEEL_DIAMETER_IN = self.ROVER_WHEEL_DIAMETER_CM / 2.54
+        self.SYS_CLK = 100000000
+        self.ticks_per_revolution = 1000 / 3.0  # From "Rover 5.pdf"
 
-	def diameter_to_circumference(self, diameter):
-		# C = 2 Pi r
-		return 2 * math.pi * (diameter / 2.0)
+    def diameter_to_circumference(self, diameter):
+        # C = 2 Pi r
+        return 2 * math.pi * (diameter / 2.0)
 
-	def ticks_to_distance(self, ticks):
-		# (Ticks / 360) * diameter
-		#return (ticks / 360.0) * self.diameter_to_circumference(self.ROVER_WHEEL_DIAMETER_MM)
-		return (ticks / self.states_per_rotation) * self.diameter_to_circumference(self.ROVER_WHEEL_DIAMETER_MM)
+    def ticks_to_distance(self, ticks):
+        # Returns distance traveled in mm
+        #return (ticks / self.ticks_per_revolution) * self.diameter_to_circumference(self.ROVER_WHEEL_DIAMETER_MM)
+        return (ticks / (self.ticks_per_revolution * self.GEAR_BOX_RATIO)) * self.diameter_to_circumference(self.ROVER_WHEEL_DIAMETER_MM)
--- team_folder/newFolder/Python/echo_count_to_distance.py
+++ team_folder/newFolder/Python/echo_count_to_distance.py
@@ -1,22 +1,22 @@
 #/usr/bin/python
 
 class EchoCalc:
-	def __init__(self):
-		self.SYS_CLK = 100000000  # 100 MHZ
-		self.SOUND_CM_S = 34300  # cm/s
+    def __init__(self):
+        self.SYS_CLK = 100000000  # 100 MHZ
+        self.SOUND_CM_S = 34300  # cm/s
 
-	def time_high(self, high_pulse_count):
-		"""
-		Convert count of high sensor pulse to a time in seconds given system clock
-		:param high_pulse_count: int
-		return: int
-		"""
-	 	return high_pulse_count / float(self.SYS_CLK)
+    def time_high(self, high_pulse_count):
+        """
+        Convert count of high sensor pulse to a time in seconds given system clock
+        :param high_pulse_count: int
+        return: int
+        """
+        return high_pulse_count / float(self.SYS_CLK)
 
-	def get_distance_cm(self, high_pulse_count):
-		"""
-		Convert high pulse count to distance in cm
-		:param high_pulse_count: int
-		return: int
-		"""
-		return self.time_high(high_pulse_count) * (self.SOUND_CM_S / 2)
+    def get_distance_cm(self, high_pulse_count):
+        """
+        Convert high pulse count to distance in cm
+        :param high_pulse_count: int
+        return: int
+        """
+        return self.time_high(high_pulse_count) * (self.SOUND_CM_S / 2)
