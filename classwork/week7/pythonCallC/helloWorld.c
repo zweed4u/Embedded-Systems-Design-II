3fabd579d3012ab20fa5dcf981202e27d6dfa6e6
Zachary Weeden <zdw7287@rit.edu>
2018-03-31 23:42:45 -0400

    Reworked gui to use wrapper c functions using shared object

--- classwork/week7/pythonCallC/helloWorld.c
+++ classwork/week7/pythonCallC/helloWorld.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-int main(){
-  printf("hello world\n");
-  foo();
-  return(0);
-}
-
 int foo(int a){
   printf("foo\n");
   printf("%d\n",a);
---
+++ lab/lab3_vhd_c_python/on_snickerdoodle/lab3.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+# Zachary Weeden 2018
+# Lab 3 Desktop GUI
+
+import os
+import sys
+from PyQt4 import QtGui
+from ctypes import cdll
+
+class MyWidget(QtGui.QWidget):
+    def __init__(self):
+        """
+        Constructor for GUI widget
+        """
+        super(MyWidget, self).__init__()
+
+        # Initialize in idle state
+        self.pwm_period_ms = 0
+        self.duty_cycle_ms = 0
+        self.enable = False
+
+        # layout stuff
+        self.pwm_label = QtGui.QLabel(self)
+        self.pwm_label.setText('PWM Period in ms:')
+
+        self.duty_label = QtGui.QLabel(self)
+        self.duty_label.setText('Duty Cycle in ms:')
+
+        self.pwm_period_int_field = QtGui.QLineEdit(self)
+        self.duty_cycle_int_field = QtGui.QLineEdit(self)
+        self.enable_label = QtGui.QLabel(self)
+
+        self.enable_label.setText('Enable:')
+        self.enable_box = QtGui.QCheckBox()
+
+        self.submit_button = QtGui.QPushButton("Submit")
+        self.exit_button = QtGui.QPushButton("Exit")
+
+        vLayout = QtGui.QVBoxLayout()
+        vLayout.addWidget(self.pwm_label)
+        vLayout.addWidget(self.pwm_period_int_field)
+        vLayout.addWidget(self.duty_label)
+        vLayout.addWidget(self.duty_cycle_int_field)
+        vLayout.addWidget(self.enable_label)
+        vLayout.addWidget(self.enable_box)
+        vLayout.addWidget(self.submit_button)
+        vLayout.addWidget(self.exit_button)
+        self.setLayout(vLayout)
+
+        # C wrapper 
+        self.pwm_lib = cdll.LoadLibrary('./pwm_drv.so')
+
+        # slots
+        self.submit_button.clicked.connect(lambda: self.set_all())
+        self.exit_button.clicked.connect(lambda: self.exit())
+        self.show()
+
+    def set_all(self):
+        """
+        Setter method invoked on submit gui button press
+        Sets class attributes used in pwm signal generation
+        :return:
+        """
+        self.pwm_period_ms = self.pwm_lib.set_period(int(self.pwm_period_int_field.text()))
+        self.duty_cycle_ms = self.pwm_lib.set_duty(int(self.duty_cycle_int_field.text()))
+        self.enable = self.pwm_lib.set_enable(int(self.enable_box.isChecked()) == True)
+
+    def exit(self):
+        """
+        Exit logic used when exit button pressed on gui
+        :return:
+        """
+        print 'State: Exitting'
+        self.stop()
+        sys.exit()
+
+    def stop(self):
+        """
+        Clear function that sets signal low and does housekeeping on status
+        variables
+        :return:
+        """
+        print 'State: Low'
+        self.blink = 0
+        self.state = 0
+
+
+def main():
+    """
+    Main of lab 3 desktop gui
+    :return:
+    """
+    os.system('cat design_1_wrapper.bin > /dev/xdevcfg')
+    app = QtGui.QApplication(sys.argv)
+    myWidget = MyWidget()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
